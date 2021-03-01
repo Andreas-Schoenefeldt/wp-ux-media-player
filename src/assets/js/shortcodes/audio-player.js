a8855213f3292076e507caedb0b06ef20d769f76
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2021-03-02 00:06:18 +0100

    Release v1.1.3

--- src/assets/js/shortcodes/audio-player.js
+++ src/assets/js/shortcodes/audio-player.js
@@ -11,16 +11,32 @@ window.onload = function () {
 
     window.setTimeout(function () {
         var frames = document.querySelectorAll('iframe');
+        var players = [];
 
-        console.log(frames);
-
+        var id = 0;
         for (var i = 0; i < frames.length; i++) {
             var iframe = frames[i];
+            iframe.id = 'id-' + id;
             var player = new Vimeo.Player(iframe);
 
             player.on('play', function() {
+
+                var currentId = parseInt(this.id.substr(3));
+                console.log(currentId);
+
                 console.log('played the video!', this);
+
+                players.forEach(function (p, index) {
+                    if (currentId !== index) {
+                        p.pause();
+                    }
+                })
+
             }.bind(iframe));
+
+            players.push(player);
+
+            id++;
         }
 
 

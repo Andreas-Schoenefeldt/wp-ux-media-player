eeffe70105641facaee5db27927ed936f20ce05c
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2021-03-01 23:48:53 +0100

    Release v1.1.0

--- src/Plugin/Plugin.php
+++ src/Plugin/Plugin.php
@@ -5,7 +5,7 @@ namespace WpUxMediaPlayer\Plugin;
 
 class Plugin {
 
-    const version = '1.0.12';
+    const version = '1.1.0';
     const plugin_name = 'wp-ux-media-player';
     const admin_page = 'wp-ux-media-player-admin';
 

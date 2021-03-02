fe84b186b2c35cb5efe8dc13bf0750008620132e
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2021-03-02 18:07:44 +0100

    Release v1.1.5

--- src/assets/js/shortcodes/audio-player.js
+++ src/assets/js/shortcodes/audio-player.js
@@ -6,9 +6,8 @@ window.onload = function () {
         widgetClass: 'js-sh-widget'
     });
 
-    // vimeo stuff
-
-
+    // vimeo stuff - froogaloop api
+    // https://stackoverflow.com/questions/6537415/vimeo-froogaloop-api-not-recognizing-an-event
     window.setTimeout(function () {
         var frames = document.querySelectorAll('iframe');
         var players = [];
@@ -16,30 +15,28 @@ window.onload = function () {
         var id = 0;
         for (var i = 0; i < frames.length; i++) {
             var iframe = frames[i];
-            iframe.id = 'id-' + id;
-            var player = Froogaloop(iframe);
+            var playerId = 'id-' + id;
+            iframe.id = playerId;
+            iframe.src = iframe.src + '&player_id=' + playerId;
+
+            var player = Froogaloop(playerId);
 
             player.addEvent('play', function() {
-
                 var currentId = parseInt(this.id.substr(3));
-                console.log(currentId);
 
-                console.log('played the video!', this);
+                console.log('playing ' + currentId);
 
                 players.forEach(function (p, index) {
                     if (currentId !== index) {
                         p.api('pause');
                     }
                 })
-
             }.bind(iframe));
-
             players.push(player);
-
             id++;
         }
 
 
-    }, 1000);
+    }, 500);
 
 };
